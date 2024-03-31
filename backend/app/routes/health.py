a4c7356713b76fb6d673d879475fcbc7bf49908a
Sanjeevan Ambalavanar <hello@sanjeevan.co.uk>
2024-03-31 23:56:39 +0100

    update health endpoint

--- backend/app/routes/health.py
+++ backend/app/routes/health.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter(tags=["Health"])
 
 
-@router.get("/", response_model=str)
-def health_index():
-    return "OK"
+@router.get("/")
+def health_index(request: Request):
+    return {
+        "health": "good",
+        "agent": request.headers.get("user-agent"),
+        "ip": request.client.host if request.client else None,
+    }
